26ee6303ee2bda19faf830e197ad93032cf7938e
Bastien Boymond <71839723+BastienBoymond@users.noreply.github.com>
2021-09-30 18:36:36 +0200

    fix(licenses): add bubble for licences and add ODBL Licences

--- packages/web-app/src/components/common/SideMenu/Footer.jsx
+++ packages/web-app/src/components/common/SideMenu/Footer.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import { Typography } from '@material-ui/core';
 import InternationalizedLink from '../InternationalizedLink';
-import { licenceLinks } from '../../../conf/Config';
+import { licenceLinks, licensesODBLink } from '../../../conf/Config';
 import GCLogo from '../GCLogo';
+import Translate from '../Translate';
 
 const LogoFooter = styled(GCLogo)`
   & > img {
@@ -17,24 +18,39 @@ const LicenceImage = styled.img`
   width: 75px;
 `;
 
+
 const Container = styled.div`
   margin-top: auto;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  align-items: baseline;
+`;
+
+const Spaced = styled.div`
+  margin-left: 10%;
+  text-align: center;
+`;
+
+const AlignText = styled.div`
+  padding-top: 10px;
+  text-align: center;
 `;
 
 const Footer = () => {
   return (
     <Container>
-      <div>
+      <AlignText>
         <LogoFooter />
-        <Typography variant="caption"> v 21.0.0 </Typography>
-      </div>
-      <InternationalizedLink links={licenceLinks}>
-        <LicenceImage src="/images/CC-BY-SA.png" alt="CC-BY-SA licence" />
-      </InternationalizedLink>
+        <Typography variant="caption"> v.21.0.7 </Typography>
+      </AlignText>
+        <Spaced>
+          <InternationalizedLink links={licensesODBLink}>
+            <LicenceImage src="/images/ODBL.png" alt="ODBL license" title="The ODBL license applies to all data that is not copyrighted"/>
+          </InternationalizedLink>
+          <InternationalizedLink links={licenceLinks}>
+            <LicenceImage src="/images/CC-BY-SA.png" alt="CC-BY-SA licence" title="Unless otherwise stated, the CC-BY-SA license applies for documents and texts subject to copyright." />
+          </InternationalizedLink>
+        </Spaced>
     </Container>
   );
 };
