fd87dce1243b71a3d2aff30bf3b527d54c90b142
Bastien Boymond <71839723+BastienBoymond@users.noreply.github.com>
2021-09-30 18:36:36 +0200

    fix(licenses): pull request modification

--- packages/web-app/src/components/common/SideMenu/Footer.jsx
+++ packages/web-app/src/components/common/SideMenu/Footer.jsx
@@ -45,7 +45,7 @@ const Footer = () => {
       </AlignText>
         <Spaced>
           <InternationalizedLink links={licensesODBLink}>
-            <LicenceImage src="/images/ODBL.png" alt="ODBL license" title="The ODBL license applies to all data that is not copyrighted"/>
+            <LicenceImage src="/images/odbl.png" alt="ODBL license" title="The ODBL license applies to all data that is not copyrighted"/>
           </InternationalizedLink>
           <InternationalizedLink links={licenceLinks}>
             <LicenceImage src="/images/CC-BY-SA.png" alt="CC-BY-SA licence" title="Unless otherwise stated, the CC-BY-SA license applies for documents and texts subject to copyright." />
