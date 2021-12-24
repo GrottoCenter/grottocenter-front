3a0fa44044daeff61e0e2202907485fc4432d11f
Clément Roig <clm.roig@gmail.com>
2021-12-24 11:30:55 +0100

    feat(#19): add more doc types

--- packages/web-app/src/components/appli/Document/DocumentForm/DocumentTypesHelper.js
+++ packages/web-app/src/components/appli/Document/DocumentForm/DocumentTypesHelper.js
@@ -1,52 +1,25 @@
 export const DocumentTypes = {
   UNKNOWN: 0,
   COLLECTION: 1,
+  DATASET: 2,
+  EVENT: 3,
   IMAGE: 4,
+  INTERACTIVE_RESOURCE: 5,
+  MOVING_IMAGE: 6,
+  PHYSICAL_OBJECT: 7,
+  SERVICE: 8,
+  SOFTWARE: 9,
+  SOUND: 10,
+  STILL_IMAGE: 11,
   TEXT: 12,
+  TOPOGRAPHIC_DRAWING: 13,
+  TOPOGRAPHIC_DATA: 14,
   BOOK: 16,
   ISSUE: 17,
-  ARTICLE: 18
+  ARTICLE: 18,
+  MAP: 19,
+  AUTHORIZATION_TO_PUBLISH: 20
 };
-
-export const allDocumentTypes = [
-  {
-    id: DocumentTypes.COLLECTION,
-    comment: 'Example: Stalactite - organ of the Swiss Speleological Society',
-    name: 'Collection',
-    parentId: null
-  },
-  // {
-  //   id: DocumentTypes.IMAGE,
-  //   comment: 'A visual representation other than text',
-  //   name: 'Image',
-  //   parentId: null,
-  // },
-  {
-    id: DocumentTypes.TEXT,
-    comment: 'All other documents that have already been published',
-    name: 'Text',
-    parentId: null
-  },
-  {
-    id: DocumentTypes.ARTICLE,
-    comment:
-      'Example: “10th International Congress of Speleology” in UIS Bulletin n°33',
-    name: 'Article',
-    parentId: DocumentTypes.TEXT
-  },
-  {
-    id: DocumentTypes.ISSUE,
-    comment: 'Example: Spelunca n°155 (September 2009)',
-    name: 'Issue',
-    parentId: DocumentTypes.TEXT
-  }
-  // {
-  //   id: DocumentTypes.BOOK,
-  //   comment: '',
-  //   name: 'Book',
-  //   parentId: DocumentTypes.TEXT,
-  // },
-];
 
 export const isUnknown = documentType => {
   return documentType.id === DocumentTypes.UNKNOWN;
