89142353d1c0db4e380e143de2489e94404887b1
Clément Roig <clm.roig@gmail.com>
2022-01-06 16:21:08 +0100

    refactor(cavesList): rewrite cave list component

--- packages/web-app/src/components/common/cave/CaveListItem.jsx
+++ packages/web-app/src/components/common/cave/CaveListItem.jsx
@@ -1,109 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import { withStyles } from '@material-ui/core';
-import { withRouter } from 'react-router-dom';
+import { Box, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const CaveDepthIcon = styled.img`
-  display: inline-block;
-  vertical-align: text-bottom;
   width: 2.5rem;
 `;
 
 const CaveLengthIcon = styled.img`
-  display: inline-block;
-  vertical-align: text-bottom;
   width: 2.5rem;
 `;
 
-const styles = theme => ({
-  caveItem: {
-    alignItems: 'flex-start',
-    flexDirection: 'column',
-    listStyleType: 'none',
-    width: '20%',
-    padding: '10px',
-    whiteSpace: 'nowrap',
-    [theme.breakpoints.down('md')]: {
-      width: '50%'
-    },
-    [theme.breakpoints.down('sm')]: {
-      width: '100%'
-    }
-  },
-  caveLink: {
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    width: '100%'
-  },
-  caveText: {
-    overflow: 'hidden',
-    padding: 0,
-    textOverflow: 'ellipsis',
-    width: '100%'
-  }
-});
+const StyledListItem = styled(ListItem)`
+  align-items: flex-start;
+  display: flex;
+  flex-basis: 25%;
+  flex-direction: column;
+  min-width: 250px;
+`;
 
-const CaveListItem = ({ classes, cave, history }) => {
-  const handleClick = () => {
-    if (cave.id) {
-      history.push(`/ui/caves/${cave.id}`);
-    }
-  };
-
+const CaveListItem = ({ cave }) => {
   return (
-    <ListItem className={classes.caveItem} button onClick={handleClick}>
-      {cave.name}
-      <ListItemText className={classes.caveText}>
-        {cave.depth ? (
-          <>
-            <CaveDepthIcon src="/images/depth.svg" alt="Cave depth icon" />
-            <span
-              style={{
-                marginRight: '5px',
-                verticalAlign: 'super'
-              }}>
-              {`${cave.depth.toLocaleString()}m`}
-            </span>
-          </>
-        ) : (
-          ''
-        )}
-        {cave.length ? (
-          <>
-            <CaveLengthIcon src="/images/length.svg" alt="Cave depth icon" />
-            <span
-              style={{
-                verticalAlign: 'super'
-              }}>
-              {`${cave.length.toLocaleString()}m`}
-            </span>
-          </>
-        ) : (
-          ''
-        )}
-      </ListItemText>
-    </ListItem>
+    <StyledListItem
+      button
+      component={React.forwardRef((props, ref) => (
+        <Link {...props} to={`/ui/caves/${cave.id}`} ref={ref} />
+      ))}
+      dense>
+      <ListItemText primary={cave.name} />
+      {(cave.depth || cave.length) && (
+        <Box display="flex" flexDirection="row" alignItems="flex-start">
+          {cave.depth !== null && (
+            <ListItem dense>
+              <ListItemIcon dense>
+                <CaveDepthIcon src="/images/depth.svg" alt="Cave depth icon" />
+              </ListItemIcon>
+              <ListItemText secondary={`${cave.depth.toLocaleString()}m`} />
+            </ListItem>
+          )}
+          {cave.length !== null && (
+            <ListItem dense>
+              <ListItemIcon dense>
+                <CaveLengthIcon
+                  src="/images/length.svg"
+                  alt="Cave length icon"
+                />
+              </ListItemIcon>
+              <ListItemText secondary={`${cave.length.toLocaleString()}m`} />
+            </ListItem>
+          )}
+        </Box>
+      )}
+    </StyledListItem>
   );
 };
 
 CaveListItem.propTypes = {
-  classes: PropTypes.shape({
-    caveItem: PropTypes.string,
-    caveText: PropTypes.string
-  }).isRequired,
   cave: PropTypes.shape({
     id: PropTypes.number,
     name: PropTypes.string,
     depth: PropTypes.number,
     length: PropTypes.number
-  }),
-  history: PropTypes.shape({ push: PropTypes.func }).isRequired
+  })
 };
 CaveListItem.defaultProps = {
   cave: undefined
 };
 
-export default withRouter(withStyles(styles)(CaveListItem));
+export default CaveListItem;
