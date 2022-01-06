9f8768e00efe73a1f1895a78236e2aed10766e3a
Clément Roig <clm.roig@gmail.com>
2022-01-06 12:29:23 +0100

    feat(#303): display massif entrances (instead of caves)

---
+++ packages/web-app/src/components/common/entrance/EntrancesList.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { List, Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import Translate from '../Translate';
+import EntranceListItem from './EntranceListItem';
+
+const StyledList = withStyles({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    width: '100%'
+  }
+})(List);
+
+const EntrancesList = props => {
+  const { entrances, title, emptyMessageComponent } = props;
+
+  return (
+    <div>
+      <Typography>{title}</Typography>
+      {entrances.length > 0 ? (
+        <StyledList>
+          {entrances
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .map(entrance => (
+              <EntranceListItem key={entrance.id} entrance={entrance} />
+            ))}
+        </StyledList>
+      ) : (
+        <>
+          <br />
+          <em>{emptyMessageComponent}</em>
+        </>
+      )}
+    </div>
+  );
+};
+
+EntrancesList.propTypes = {
+  entrances: PropTypes.arrayOf(PropTypes.shape({})),
+  title: PropTypes.node,
+  emptyMessageComponent: PropTypes.node
+};
+
+EntrancesList.defaultProps = {
+  entrances: undefined,
+  title: (
+    <strong>
+      <Translate>Entrances list</Translate>
+    </strong>
+  ),
+  emptyMessageComponent: <Translate>Empty list</Translate>
+};
+
+export default EntrancesList;
