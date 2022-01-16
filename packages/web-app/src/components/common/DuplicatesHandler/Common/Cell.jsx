173e78c7a0a46f35d7baa1a566544aed2d0e0aab
Axel <axelcanton@hotmail.com>
2022-01-16 18:05:39 +0100

    feat(duplicateTool): add reusable component

---
+++ packages/web-app/src/components/common/DuplicatesHandler/Common/Cell.jsx
@@ -0,0 +1,149 @@
+import React from 'react';
+import { Fab, Grid, InputAdornment, useTheme } from '@material-ui/core';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import PropTypes from 'prop-types';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+import StringInput from '../../Form/StringInput';
+import { MarginLeftDiv, MarginRightDiv } from './WrapperUtilities';
+import AdornementPlaceholder from './AdornementPlaceholder';
+
+export const RightCell = ({
+  value,
+  label,
+  updateState,
+  render,
+  disableButton,
+  showAdornment,
+  severity
+}) => {
+  const theme = useTheme();
+  let iconColor;
+  switch (severity) {
+    case 'primary':
+      iconColor = theme.palette.successColor;
+      break;
+    case 'secondary':
+      iconColor = theme.palette.errorColor;
+      break;
+    default:
+      iconColor = theme.palette.secondary.light;
+  }
+  return (
+    <Grid container direction="row" justify="flex-end" alignItems="center">
+      <MarginRightDiv>
+        <Fab
+          onClick={() => updateState(value)}
+          color="primary"
+          size="small"
+          disabled={disableButton(value)}>
+          <ArrowBackIosIcon />
+        </Fab>
+      </MarginRightDiv>
+      <StringInput
+        value={render(value)}
+        valueName={label}
+        disabled
+        fullWidth={false}
+        endAdornment={
+          showAdornment ? (
+            <InputAdornment position="end">
+              <ErrorOutlineIcon style={{ color: iconColor }} />
+            </InputAdornment>
+          ) : (
+            <AdornementPlaceholder />
+          )
+        }
+      />
+    </Grid>
+  );
+};
+
+export const MiddleCell = ({ value, label, updateState, render, disabled }) => {
+  return (
+    <StringInput
+      value={render(value)}
+      valueName={label}
+      onValueChange={updateState}
+      disabled={disabled}
+      fullWidth
+    />
+  );
+};
+
+export const LeftCell = ({
+  value,
+  label,
+  updateState,
+  render,
+  disableButton,
+  showAdornment,
+  severity
+}) => {
+  const theme = useTheme();
+  let iconColor;
+  switch (severity) {
+    case 'primary':
+      iconColor = theme.palette.successColor;
+      break;
+    case 'secondary':
+      iconColor = theme.palette.errorColor;
+      break;
+    default:
+      iconColor = theme.palette.secondary.light;
+  }
+  return (
+    <Grid container direction="row" justify="flex-start" alignItems="center">
+      <StringInput
+        value={render(value)}
+        valueName={label}
+        disabled
+        fullWidth={false}
+        startAdornment={
+          showAdornment ? (
+            <InputAdornment position="start">
+              <ErrorOutlineIcon style={{ color: iconColor }} />
+            </InputAdornment>
+          ) : (
+            <AdornementPlaceholder />
+          )
+        }
+      />
+      <MarginLeftDiv>
+        <Fab
+          onClick={() => updateState(value)}
+          color="primary"
+          size="small"
+          disabled={disableButton(value)}>
+          <ArrowForwardIosIcon />
+        </Fab>
+      </MarginLeftDiv>
+    </Grid>
+  );
+};
+
+RightCell.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
+  render: PropTypes.func,
+  updateState: PropTypes.func.isRequired,
+  disableButton: PropTypes.bool,
+  showAdornment: PropTypes.bool.isRequired,
+  severity: PropTypes.oneOf(['primary', 'secondary'])
+};
+LeftCell.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
+  render: PropTypes.func,
+  updateState: PropTypes.func.isRequired,
+  disableButton: PropTypes.bool,
+  showAdornment: PropTypes.bool.isRequired,
+  severity: PropTypes.oneOf(['primary', 'secondary'])
+};
+MiddleCell.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
+  render: PropTypes.func,
+  updateState: PropTypes.func.isRequired,
+  disabled: PropTypes.bool
+};
---
+++ packages/web-app/src/components/common/DuplicatesHandler/DocumentsHandler.jsx
@@ -0,0 +1,261 @@
+/* eslint-disable react/forbid-prop-types */
+import React, { useState, useCallback, useMemo } from 'react';
+import { Grid } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { isEmpty } from 'ramda';
+import GridLine from './Common/GridLine';
+import TitleLine from './Common/TitleLine';
+import ActionLine from './Common/ActionLine';
+import { getDocumentSchema } from './utils/getSchema';
+import GridLineCollection from './Common/GridLineCollection';
+import shouldLineRender from './utils/shouldLineRender';
+import {
+  getIdOrUndefined,
+  retrieveFromObjectCollection
+} from './utils/retrieveFromObjectCollection';
+
+const DocumentsHandler = ({
+  duplicate1,
+  duplicate2,
+  handleSubmit,
+  handleNotDuplicatesSubmit,
+  title1,
+  title2
+}) => {
+  const [author, setAuthor] = useState('');
+  const [reviewer, setReviewer] = useState('');
+  const [dateInscription, setDateInscription] = useState('');
+  const [datePublication, setDatePublication] = useState('');
+  const [dateReviewed, setDateReviewed] = useState('');
+  const [authorComment, setauthorComment] = useState('');
+  const [identifier, setIdentifier] = useState('');
+  const [identifierType, setIdentifierType] = useState('');
+  const [refBbs, setRefBbs] = useState('');
+  const [entrance, setEntrance] = useState('');
+  const [cave, setCave] = useState('');
+  const [massif, setMassif] = useState('');
+  const [editor, setEditor] = useState('');
+  const [library, setLibrary] = useState('');
+  const [pages, setPages] = useState('');
+  const [type, setType] = useState('');
+  const [parent, setParent] = useState('');
+  const [license, setLicense] = useState('');
+  const [authors, setAuthors] = useState([]);
+  const [subjects, setSubjects] = useState([]);
+  const [titles, setTitles] = useState([]);
+  const [descriptions, setDescriptions] = useState([]);
+  const [languages, setLanguages] = useState([]);
+
+  const documentsSchema = useMemo(() => getDocumentSchema(), []);
+
+  // eslint-disable-next-line consistent-return
+  const getStateData = (attribute, getState) => {
+    switch (attribute) {
+      case 'author':
+        return getState ? author : setAuthor;
+      case 'reviewer':
+        return getState ? reviewer : setReviewer;
+      case 'datePublication':
+        return getState ? datePublication : setDatePublication;
+      case 'authorComment':
+        return getState ? authorComment : setauthorComment;
+      case 'identifier':
+        return getState ? identifier : setIdentifier;
+      case 'identifierType':
+        return getState ? identifierType : setIdentifierType;
+      case 'refBbs':
+        return getState ? refBbs : setRefBbs;
+      case 'entrance':
+        return getState ? entrance : setEntrance;
+      case 'pages':
+        return getState ? pages : setPages;
+      case 'dateInscription':
+        return getState ? dateInscription : setDateInscription;
+      case 'dateReviewed':
+        return getState ? dateReviewed : setDateReviewed;
+      case 'massif':
+        return getState ? massif : setMassif;
+      case 'editor':
+        return getState ? editor : setEditor;
+      case 'library':
+        return getState ? library : setLibrary;
+      case 'cave':
+        return getState ? cave : setCave;
+      case 'type':
+        return getState ? type : setType;
+      case 'intactDescriptions':
+        return getState ? descriptions : setDescriptions;
+      case 'parent':
+        return getState ? parent : setParent;
+      case 'license':
+        return getState ? license : setLicense;
+      case 'authors':
+        return getState ? authors : setAuthors;
+      case 'subjects':
+        return getState ? subjects : setSubjects;
+      case 'titles':
+        return getState ? titles : setTitles;
+      case 'languages':
+        return getState ? languages : setLanguages;
+      default:
+    }
+  };
+
+  const isSubmittable = () => {
+    return !(isEmpty(author) || isEmpty(type) || isEmpty(license));
+  };
+
+  const onSubmit = () => {
+    const {
+      newItems: newAuthors,
+      previousItems: previousAuthors
+    } = retrieveFromObjectCollection(authors);
+
+    const {
+      newItems: newPopulatedDescriptions,
+      previousItems: previousDescriptions
+    } = retrieveFromObjectCollection(descriptions);
+    const newDescriptions = newPopulatedDescriptions.map(newDesc => ({
+      ...newDesc,
+      language: newDesc.language.id
+    }));
+    handleSubmit(
+      {
+        id: duplicate1.id || duplicate2.id,
+        author: author.id,
+        reviewer: getIdOrUndefined(entrance),
+        dateInscription,
+        datePublication,
+        dateReviewed,
+        authorComment,
+        identifier,
+        identifierType: getIdOrUndefined(identifierType),
+        refBbs,
+        entrance: getIdOrUndefined(entrance),
+        cave: getIdOrUndefined(cave),
+        massif: getIdOrUndefined(massif),
+        editor: getIdOrUndefined(parent),
+        pages,
+        library: getIdOrUndefined(entrance),
+        type: type.id,
+        parent: getIdOrUndefined(parent),
+        license: license.id,
+        authors: previousAuthors,
+        subjects: subjects.map(sub => sub.code),
+        descriptions: previousDescriptions,
+        languages: languages.map(lang => lang.id)
+      },
+      {
+        newAuthors,
+        newDescriptions
+      }
+    );
+  };
+
+  const onNotDuplicatesSubmit = () => {
+    handleNotDuplicatesSubmit({
+      author,
+      reviewer,
+      dateInscription,
+      datePublication,
+      dateReviewed,
+      authorComment,
+      identifier,
+      identifierType,
+      refBbs,
+      entrance,
+      cave,
+      massif,
+      editor,
+      pages,
+      library,
+      type,
+      parent,
+      license,
+      authors,
+      subjects,
+      titles,
+      descriptions,
+      languages
+    });
+  };
+
+  const handleAllClick = useCallback(
+    duplicate => {
+      documentsSchema.forEach(element => {
+        const value = duplicate[element.attribute];
+        if (value) {
+          getStateData(element.attribute, false)(value);
+        }
+      });
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [duplicate1, duplicate2]
+  );
+
+  return (
+    <Grid container direction="column" alignItems="center">
+      <TitleLine
+        title1={title1}
+        title2={title2}
+        handleAllClick1={() => handleAllClick(duplicate1)}
+        handleAllClick2={() => handleAllClick(duplicate2)}
+      />
+      <ActionLine
+        handleSubmit={onSubmit}
+        handleNotDuplicatesSubmit={onNotDuplicatesSubmit}
+        disableSubmit={!isSubmittable()}
+      />
+      {documentsSchema.map(line => {
+        if (
+          shouldLineRender(
+            line.isCollection,
+            duplicate1[line.attribute],
+            duplicate2[line.attribute]
+          )
+        ) {
+          return line.isCollection ? (
+            <GridLineCollection
+              key={line.label}
+              label={line.label}
+              value1={duplicate1[line.attribute]}
+              value2={duplicate2[line.attribute]}
+              render={line.customRender}
+              stateValue={getStateData(line.attribute, true)}
+              updateState={getStateData(line.attribute, false)}
+              disabled={line.disabled}
+            />
+          ) : (
+            <GridLine
+              key={line.label}
+              label={line.label}
+              value1={duplicate1[line.attribute]}
+              value2={duplicate2[line.attribute]}
+              render={line.customRender}
+              stateValue={getStateData(line.attribute, true)}
+              updateState={getStateData(line.attribute, false)}
+              disabled={line.disabled}
+            />
+          );
+        }
+        return <></>;
+      })}
+      <ActionLine
+        handleSubmit={onSubmit}
+        handleNotDuplicatesSubmit={onNotDuplicatesSubmit}
+        disableSubmit={!isSubmittable()}
+      />
+    </Grid>
+  );
+};
+
+export default DocumentsHandler;
+
+DocumentsHandler.propTypes = {
+  duplicate1: PropTypes.object.isRequired,
+  duplicate2: PropTypes.object.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleNotDuplicatesSubmit: PropTypes.func.isRequired,
+  title1: PropTypes.string,
+  title2: PropTypes.string
+};
