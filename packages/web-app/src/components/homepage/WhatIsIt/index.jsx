a87eeb99386831c2fe24ac46f1330fd4a2584038
urienf <frederic.urien@univ-grenoble-alpes.fr>
2022-04-11 13:56:22 +0200

    Fix

--- packages/web-app/src/components/homepage/WhatIsIt/index.jsx
+++ packages/web-app/src/components/homepage/WhatIsIt/index.jsx
@@ -92,14 +92,11 @@ const WhatIsIt = () => (
     <GridRow>
       <NumberBlockHalfWidth>
         <BlockIcon className="icon-gc-entries" />
-        <BigNumberText numberType="publicEntrances" />
+        <BigNumberText numberType="entrances" />
         <br />
         <Translate>
-          caves are freely accessible from the following page (
+          caves are freely accessible
         </Translate>
-        <NumberText numberType="entrances" />{' '}
-        <Translate>by logging on</Translate>{' '}
-        <GCLink href="http://www.grottocenter.org">Grotto v2</GCLink>)
       </NumberBlockHalfWidth>
       <NumberBlockHalfWidth>
         <BlockIcon className="icon-gc-club" />
