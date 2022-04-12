26ddfd55b27d51e193f20ef293bc54dde4b12046
Clement <clm.roig@gmail.com>
2022-04-12 09:58:16 +0200

    feat(apiDoc): display api doc, clean and style api page

--- packages/web-app/src/pages/ApiDoc.jsx
+++ packages/web-app/src/pages/ApiDoc.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ApiDetail from '../components/appli/ApiDetail';
 
-const Swagger = props => {
+const ApiDoc = props => {
   const {
     match: {
       params: { version }
@@ -11,7 +11,7 @@ const Swagger = props => {
   return <ApiDetail version={Number.parseInt(version, 10)} />;
 };
 
-Swagger.propTypes = {
+ApiDoc.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
       version: PropTypes.string
@@ -19,4 +19,4 @@ Swagger.propTypes = {
   }).isRequired
 };
 
-export default Swagger;
+export default ApiDoc;
