731dba35e3e36447b49c72645026ba9a2ac1f46d
VivienLvr <levacher.vivien@gmail.com>
2022-05-12 16:54:13 +0200

    feat(Organization): members list

---
+++ packages/web-app/src/components/common/UsersList/UsersList.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { List, Typography } from '@material-ui/core';
+import styled from 'styled-components';
+import Translate from '../Translate';
+import OrganizationListItem from './UsersListItem';
+
+const StyledList = styled(List)({
+  display: 'flex',
+  flexWrap: 'wrap',
+  width: '100%'
+});
+
+const UsersList = props => {
+  const { users, title, emptyMessageComponent } = props;
+
+  return (
+    <>
+      {title && (
+        <Typography variant="h3" gutterBottom>
+          {title}
+        </Typography>
+      )}
+      {users && users.length > 0 ? (
+        <StyledList>
+          {users
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .map(user => (
+              <OrganizationListItem key={user.id} user={user} />
+            ))}
+        </StyledList>
+      ) : (
+        emptyMessageComponent
+      )}
+    </>
+  );
+};
+
+UsersList.propTypes = {
+  users: PropTypes.arrayOf(PropTypes.shape({})),
+  title: PropTypes.node,
+  emptyMessageComponent: PropTypes.node
+};
+
+UsersList.defaultProps = {
+  users: [],
+  emptyMessageComponent: <Translate>Empty list</Translate>
+};
+
+export default UsersList;
