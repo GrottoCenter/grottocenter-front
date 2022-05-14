1a9eac51cd86b4034146f3d6dab62222f6bc053b
VivienLvr <levacher.vivien@gmail.com>
2022-05-14 13:45:41 +0200

    feat(Organization): add edit button

--- packages/web-app/src/components/appli/Organization/Details.jsx
+++ packages/web-app/src/components/appli/Organization/Details.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Marker, Popup } from 'react-leaflet';
 import PropTypes from 'prop-types';
 import { isNil } from 'ramda';
-import { Typography } from '@material-ui/core';
+import { Box, Typography, IconButton } from '@material-ui/core';
 import styled from 'styled-components';
 import EmailIcon from '@material-ui/icons/Email';
 import LocationIcon from '@material-ui/icons/LocationOn';
+import CreateIcon from '@material-ui/icons/Create';
+import { useIntl } from 'react-intl';
 
 import CustomMapContainer from '../../common/Maps/common/MapContainer';
 import OrganizationMarker from '../../common/Maps/common/Markers/Components/OrganizationMarker';
@@ -35,33 +37,57 @@ const Details = ({
   region,
   village,
   position,
-  organization
+  organization,
+  // onEdit ### À décommenter lorsque la page edit sera prête
+  canEdit
 }) => {
+  const { formatMessage } = useIntl();
+
   return (
     <>
-      <ContentWrapper>
-        <StyledLocationIcon color="primary" />
-        <Typography>
-          {!isNil(country) && `${country}`}
-          {!isNil(region) && ` - ${region}`}
-          {!isNil(county) && ` - ${county}`}
-          {!isNil(city) && ` - ${city}`}
-          {!isNil(village) && ` - ${village}`}
-          {!isNil(postalCode) && ` - ${postalCode}`}
-          {!isNil(address) && ` - ${address}`}
-        </Typography>
-      </ContentWrapper>
-      {mail && (
-        <ContentWrapper>
-          <StyledEmailIcon color="primary" />
-          <Typography>{mail}</Typography>
-        </ContentWrapper>
-      )}
-      {customMessage && (
-        <ContentWrapper>
-          <Typography>{customMessage}</Typography>
-        </ContentWrapper>
-      )}
+      <Box
+        alignItems="start"
+        display="flex"
+        flexBasis="200px"
+        justifyContent="space-between">
+        <Box display="block">
+          <ContentWrapper>
+            <StyledLocationIcon color="primary" />
+            <Typography>
+              {!isNil(country) && `${country}`}
+              {!isNil(region) && ` - ${region}`}
+              {!isNil(county) && ` - ${county}`}
+              {!isNil(city) && ` - ${city}`}
+              {!isNil(village) && ` - ${village}`}
+              {!isNil(postalCode) && ` - ${postalCode}`}
+              {!isNil(address) && ` - ${address}`}
+            </Typography>
+          </ContentWrapper>
+          {mail && (
+            <ContentWrapper>
+              <StyledEmailIcon color="primary" />
+              <Typography>{mail}</Typography>
+            </ContentWrapper>
+          )}
+          {customMessage && (
+            <ContentWrapper>
+              <Typography>{customMessage}</Typography>
+            </ContentWrapper>
+          )}
+        </Box>
+        {canEdit && (
+          <IconButton
+            size="medium"
+            aria-label="edit"
+            color="primary"
+            // onClick={onEdit} ### À décommenter lorsque la page edit sera prête
+            disabled // ={isNil(onEdit)} ### À décommenter lorsque la page edit sera prête
+          >
+            {formatMessage({ id: 'Edit' })}
+            <CreateIcon />
+          </IconButton>
+        )}
+      </Box>
       <br />
       {position && (
         <>
@@ -95,7 +121,9 @@ Details.propTypes = {
   postalCode: PropTypes.string,
   village: PropTypes.string,
   position: PropTypes.arrayOf(PropTypes.number),
-  organization: PropTypes.shape({})
+  organization: PropTypes.shape({}),
+  // onEdit: PropTypes.func.isRequired, ### À décommenter lorsque la page edit sera prête
+  canEdit: PropTypes.bool.isRequired
 };
 
 export default Details;
--- packages/web-app/src/pages/Organization/index.jsx
+++ packages/web-app/src/pages/Organization/index.jsx
@@ -1,28 +1,39 @@
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { loadOrganization } from '../actions/Organization';
-import { setPageTitle } from '../actions/PageTitle';
-import Organization from '../components/appli/Organization';
+import { loadOrganization } from '../../actions/Organization';
+import { setPageTitle } from '../../actions/PageTitle';
+import Organization from '../../components/appli/Organization';
+import { usePermissions } from '../../hooks';
 
 const OrganizationPage = () => {
   const { organizationId } = useParams();
   const dispatch = useDispatch();
+  const permissions = usePermissions();
   const { organization, error, isLoading } = useSelector(
     state => state.organization
   );
+
+  const canEdit = permissions.isAuth;
+
+  const history = useHistory();
 
   useEffect(() => {
     dispatch(loadOrganization(organizationId));
     dispatch(setPageTitle('Organization'));
   }, [dispatch, organizationId]);
 
+  const onEdit = () => {
+    history.push(`/ui/organizations/edit/${organizationId}`);
+  };
   return (
     <Organization
       error={error}
       isLoading={isLoading}
       organization={organization}
+      onEdit={onEdit}
+      canEdit={canEdit}
     />
   );
 };
