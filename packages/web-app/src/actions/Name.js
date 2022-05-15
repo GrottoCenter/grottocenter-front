624f2533299882d4bbb8a723fa54d18db1c587b8
Axel Laget <axel.laget@gmail.com>
2022-05-15 14:47:06 +0200

    fix(340): edit reducer

---
+++ packages/web-app/src/actions/Name.js
@@ -0,0 +1,51 @@
+import fetch from 'isomorphic-fetch';
+import { patchNameUrl } from '../conf/Config';
+import makeErrorMessage from '../helpers/makeErrorMessage';
+
+export const UPDATE_NAME = 'UPDATE_NAME';
+export const UPDATE_NAME_SUCCESS = 'UPDATE_NAME_SUCCESS';
+export const UPDATE_NAME_FAILURE = 'UPDATE_NAME_FAILURE';
+
+export const updateNameAction = () => ({
+  type: UPDATE_NAME
+});
+export const updateNameSuccess = name => ({
+  name,
+  type: UPDATE_NAME_SUCCESS
+});
+export const updateNameFailure = (error, httpCode) => ({
+  type: UPDATE_NAME_FAILURE,
+  error,
+  httpCode
+});
+
+export const updateName = data => {
+  return (dispatch, getState) => {
+    dispatch(updateNameAction());
+
+    const requestOptions = {
+      method: 'PATCH',
+      body: JSON.stringify(data),
+      headers: getState().login.authorizationHeader
+    };
+
+    return fetch(patchNameUrl(data.id), requestOptions)
+      .then(response => {
+        if (response.status >= 400) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(res => {
+        dispatch(updateNameSuccess(res));
+      })
+      .catch(error =>
+        dispatch(
+          updateNameFailure(
+            makeErrorMessage(error.message, `Bad request`),
+            error.message
+          )
+        )
+      );
+  };
+};
