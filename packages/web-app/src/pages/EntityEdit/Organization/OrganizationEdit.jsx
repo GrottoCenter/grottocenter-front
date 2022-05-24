d599f475e0a90500fadfd72f3957a0fe79582fa4
Axel Laget <axel.laget@gmail.com>
2022-05-24 13:57:43 +0200

    fix(341): merge edit and editContainer

--- packages/web-app/src/pages/EntityEdit/Organization/OrganizationEdit.jsx
+++ packages/web-app/src/pages/EntityEdit/Organization/OrganizationEdit.jsx
@@ -1,12 +1,20 @@
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useIntl } from 'react-intl';
+import { isNil } from 'ramda';
+import PropTypes from 'prop-types';
+import { CircularProgress } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import OrganizationForm from '../../../components/appli/EntitiesForm/Organization';
+import { makeOrganizationValueData } from '../../../components/appli/EntitiesForm/Organization/transformers';
+import Layout from '../../../components/common/Layouts/Fixed/FixedContent';
+import Translate from '../../../components/common/Translate';
 import { loadOrganization } from '../../../actions/Organization';
-import OrganizationEditContainer from './OrganizationEditContainer';
 
 const OrganizationEdit = () => {
   const { organizationId } = useParams();
   const dispatch = useDispatch();
+  const { formatMessage } = useIntl();
 
   const { organization, isFetching, error } = useSelector(
     state => state.organization
@@ -15,14 +23,40 @@ const OrganizationEdit = () => {
   useEffect(() => {
     dispatch(loadOrganization(organizationId));
   }, [organizationId, dispatch]);
+  if (!isNil(error)) {
+    return (
+      <Translate>
+        Error, the organization data you are looking for is not available.
+      </Translate>
+    );
+  }
 
   return (
-    <OrganizationEditContainer
-      isFetching={isFetching}
-      organization={organization}
-      error={error}
+    <Layout
+      title={
+        organization?.name ||
+        formatMessage({ id: 'Loading the organization data...' })
+      }
+      content={
+        !isFetching && !organization ? (
+          <CircularProgress />
+        ) : (
+          <>
+            <OrganizationForm
+              organizationValues={makeOrganizationValueData(organization)}
+            />
+          </>
+        )
+      }
     />
   );
+};
+
+OrganizationEdit.propTypes = {
+  organization: PropTypes.shape({
+    name: PropTypes.shape({ message: PropTypes.string })
+  }),
+  error: PropTypes.shape({})
 };
 
 export default OrganizationEdit;
