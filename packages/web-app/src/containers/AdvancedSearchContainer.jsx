1c010bdf5d29fef8a66310412c681e2e400b40a3
Clément Roig <clm.roig@gmail.com>
2022-07-08 11:27:35 +0200

    refactor(advSearch): refactor data formatting

--- packages/web-app/src/containers/AdvancedSearchContainer.jsx
+++ packages/web-app/src/containers/AdvancedSearchContainer.jsx
@@ -8,39 +8,7 @@ import { loadSubjects } from '../actions/Subject';
 import AdvancedSearch from '../components/appli/AdvancedSearch';
 
 const startAdvancedsearch = (formValues, resourceType) => dispatch => {
-  // complete is set to true because we need the complete results about the data
-  // resourceType is set to "entrances", "grottos", "massifs" or "documents"
-  // according to the search desired
-  const paramsToSend = {
-    complete: true,
-    resourceType
-  };
-
-  Object.keys(formValues).forEach(key => {
-    let keyValue = formValues[key];
-
-    // If String trim it
-    if (typeof formValues[key] === 'string') {
-      keyValue = formValues[key].trim();
-    }
-
-    // Handle range values
-    if (keyValue !== '' && key.split('-range').length === 1) {
-      paramsToSend[key] = keyValue;
-
-      // If the key contains '-range' and it is editable
-      // then we send the parameter in two parameters min and max
-    } else if (key.split('-range').length > 1 && keyValue.isEditable === true) {
-      const keyBase = key.split('-range');
-      const rangeMin = `${keyBase[0]}-min`;
-      const rangeMax = `${keyBase[0]}-max`;
-
-      paramsToSend[rangeMin] = keyValue.min;
-      paramsToSend[rangeMax] = keyValue.max;
-    }
-  });
-
-  dispatch(fetchAdvancedsearchResults(paramsToSend));
+  dispatch(fetchAdvancedsearchResults(formValues, resourceType));
 };
 
 const resetAdvancedSearch = () => dispatch => {
