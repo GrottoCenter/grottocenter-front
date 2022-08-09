5f3cbb834f4dffc37e4944bfe5f02cc79713f26e
Clément Roig <clm.roig@gmail.com>
2022-08-09 10:45:53 +0200

    refactor(multilines): refactor styling of multilines data

---
+++ packages/web-app/src/components/common/MultilinesTypography/index.js
@@ -0,0 +1,8 @@
+import { Typography } from '@material-ui/core';
+import styled from 'styled-components';
+
+const MultilinesTypography = styled(Typography)({
+  whiteSpace: 'pre-wrap'
+});
+
+export default MultilinesTypography;
