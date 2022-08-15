c21d2e9fdbc4bbd6a2059acd1ce2efcf2e034186
Clement <clm.roig@gmail.com>
2022-08-15 18:46:27 +0200

    refactor(contribution): make contribution and author/date reusable

---
+++ packages/web-app/src/components/common/Contribution/AuthorAndDate.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { isNil } from 'ramda';
+import PropTypes from 'prop-types';
+import { Typography } from '@material-ui/core';
+import { useIntl } from 'react-intl';
+import AuthorLink from '../AuthorLink/index';
+
+const AuthorAndDate = ({ author, textColor, date, verb }) => {
+  const { formatDate } = useIntl();
+  return (
+    <Typography component="span" variant="caption" color={textColor}>
+      <AuthorLink author={author} verb={verb} />
+      {`${
+        !isNil(date)
+          ? ` - ${formatDate(date, {
+              year: 'numeric',
+              month: 'numeric',
+              day: 'numeric',
+              hour: 'numeric',
+              minute: 'numeric'
+            })}`
+          : ''
+      }`}
+    </Typography>
+  );
+};
+
+AuthorAndDate.propTypes = {
+  author: authorType,
+  date: PropTypes.string,
+  textColor: PropTypes.oneOf([
+    'textPrimary',
+    'textSecondary',
+    'inherit',
+    'initial',
+    'primary',
+    'secondary',
+    'error'
+  ]),
+  verb: PropTypes.string
+};
+
+AuthorAndDate.defaultProps = {
+  textColor: 'textPrimary'
+};
+
+export default AuthorAndDate;
