b311d38e4796bcfbcba095973c33732512bb6f51
Vincent Marseguerra <v.marseguerra@gmail.com>
2022-12-01 23:37:20 +0100

    fix(login): logout user on unauthorized response

--- packages/web-app/src/actions/Notifications/CountUnreadNotifications.js
+++ packages/web-app/src/actions/Notifications/CountUnreadNotifications.js
@@ -2,6 +2,7 @@ import fetch from 'isomorphic-fetch';
 import { countUnreadNotificationsUrl } from '../../conf/apiRoutes';
 import makeErrorMessage from '../../helpers/makeErrorMessage';
 import { checkAndGetStatus } from '../utils';
+import { logout } from '../Login';
 
 export const COUNT_UNREAD_NOTIFICATIONS = 'COUNT_UNREAD_NOTIFICATIONS';
 export const COUNT_UNREAD_NOTIFICATIONS_SUCCESS =
@@ -38,13 +39,21 @@ export function countUnreadNotifications() {
       .then(data => {
         dispatch(countUnreadNotificationsActionSuccess(data.count));
       })
-      .catch(error =>
+      .catch(error => {
+        // When logged, countUnreadNotifications() is the first authenticated request made to the API
+        // So it is the first request that test if the JWT is really valid
+        // In case of invalid token we logout the user
+        if (error.message === '401') {
+          dispatch(logout());
+          return;
+        }
+
         dispatch(
           countUnreadNotificationsActionFailure(
             makeErrorMessage(error.message, `Counting unread notifications`),
             error.message
           )
-        )
-      );
+        );
+      });
   };
 }
