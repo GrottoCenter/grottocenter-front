557c6dc70673243a74dbb13c5b2bb8c12338e412
bsoufflet <benjamin@fruitionsciences.com>
2023-03-22 00:50:14 +0100

    fix(entrance): broken "all revisions" page due to translation
    fix #891

--- packages/web-app/src/components/appli/Entry/Snapshots/AccordionSnapshotListPage.jsx
+++ packages/web-app/src/components/appli/Entry/Snapshots/AccordionSnapshotListPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useIntl } from 'react-intl';
 import PropTypes from 'prop-types';
 
 import ScrollableContent from '../../../common/Layouts/Fixed/ScrollableContent';
@@ -7,44 +6,45 @@ import getAuthor from '../../../../util/getAuthor';
 import Alert404 from './error/404Alert';
 import authorType from '../../../../types/author.type';
 import AccordionSnapshot from './AccordionSnapshot';
+import Translate from '../../../common/Translate';
 
-const AccordionSnapshotListPage = ({ data, type, isNetwork }) => {
-  const { formatMessage } = useIntl();
-  return (
-    <ScrollableContent
-      dense
-      title={formatMessage(
-        {
-          defaultMessage: 'Full {type} revision history'
-        },
-        { type }
-      )}
-      content={
-        data ? (
-          data.map(snapshotType =>
-            snapshotType[Object.keys(snapshotType)[0]].map(snapshot => {
-              const author = getAuthor(snapshot.author);
-              const reviewer = getAuthor(snapshot.reviewer);
-              return (
-                <AccordionSnapshot
-                  key={snapshot.id + snapshot.t_id}
-                  snapshot={snapshot}
-                  snapshotType={Object.keys(snapshotType)[0]}
-                  isNetwork={isNetwork}
-                  author={author}
-                  reviewer={reviewer}
-                  all
-                />
-              );
-            })
-          )
-        ) : (
-          <Alert404 type={type} />
+const AccordionSnapshotListPage = ({ data, type, isNetwork }) => (
+  <ScrollableContent
+    dense
+    title={
+      <Translate
+        id="Full {type} revision history"
+        values={{
+          type
+        }}
+        defaultMessage={`Full ${type} revision history`}
+      />
+    }
+    content={
+      data ? (
+        data.map(snapshotType =>
+          snapshotType[Object.keys(snapshotType)[0]].map(snapshot => {
+            const author = getAuthor(snapshot.author);
+            const reviewer = getAuthor(snapshot.reviewer);
+            return (
+              <AccordionSnapshot
+                key={snapshot.id + snapshot.t_id}
+                snapshot={snapshot}
+                snapshotType={Object.keys(snapshotType)[0]}
+                isNetwork={isNetwork}
+                author={author}
+                reviewer={reviewer}
+                all
+              />
+            );
+          })
         )
-      }
-    />
-  );
-};
+      ) : (
+        <Alert404 type={type} />
+      )
+    }
+  />
+);
 
 AccordionSnapshotListPage.propTypes = {
   data: PropTypes.arrayOf(
