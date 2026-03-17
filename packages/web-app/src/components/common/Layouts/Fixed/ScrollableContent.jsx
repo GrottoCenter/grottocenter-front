d96bfef58faef1f90bfb99493687498eedac00eb
Paul AUBERTIN <paul.aubertin.fr@gmail.com>
2026-03-18 00:25:12 +0100

    refactor: remove unused props from scrollable content

--- packages/web-app/src/components/common/Layouts/Fixed/ScrollableContent.jsx
+++ packages/web-app/src/components/common/Layouts/Fixed/ScrollableContent.jsx
@@ -1,17 +1,13 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { useIntl } from 'react-intl';
 import { styled } from '@mui/material/styles';
 import { isNil } from 'ramda';
 import {
   Card as MuiCard,
-  CardActions,
   CardContent,
   CardHeader as MuiCardHeader,
-  IconButton as MuiIconButton,
   Typography
 } from '@mui/material';
-import CreateIcon from '@mui/icons-material/Create';
 import AnchorCopyButton, { AnchorHeadingWrapper } from '../../AnchorCopyButton';
 import { useAnchorScroll } from '../../../../hooks';
 
@@ -19,10 +15,6 @@ const Card = styled(MuiCard)`
   overflow: inherit;
   margin: ${({ theme }) => theme.spacing(2)};
   scroll-margin-top: ${({ theme }) => theme.appBarHeight}px;
-`;
-
-const IconButton = styled(MuiIconButton)`
-  margin-left: auto;
 `;
 
 const Title = styled('div')`
@@ -36,26 +28,7 @@ const CardHeader = styled(MuiCardHeader, {
   ${({ $dense }) => $dense && `padding-bottom: 0px`}
 `;
 
-const Footer = ({ content }) =>
-  typeof content === 'string' ? (
-    <Typography variant="caption" align="right">
-      content
-    </Typography>
-  ) : (
-    content
-  );
-
-const ScrollableContent = ({
-  title,
-  icon,
-  onEdit,
-  content,
-  footer,
-  dense = false,
-  subTitle = false,
-  anchorId
-}) => {
-  const { formatMessage } = useIntl();
+const ScrollableContent = ({ title, icon, content, anchorId, dense = false, subTitle = false }) => {
   useAnchorScroll(anchorId);
 
   return (
@@ -81,18 +54,6 @@ const ScrollableContent = ({
         }
       />
       <CardContent>{content}</CardContent>
-      {!isNil(footer) && (
-        <CardActions disableSpacing>
-          <Footer content={footer} />
-          <IconButton
-            size="small"
-            aria-label={formatMessage({ id: 'edit' })}
-            disabled={isNil(onEdit)}
-            onClick={onEdit}>
-            <CreateIcon />
-          </IconButton>
-        </CardActions>
-      )}
     </Card>
   );
 };
@@ -100,16 +61,10 @@ const ScrollableContent = ({
 ScrollableContent.propTypes = {
   title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
   icon: PropTypes.node,
-  onEdit: PropTypes.func,
   content: PropTypes.node.isRequired,
-  footer: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  anchorId: PropTypes.string,
   dense: PropTypes.bool,
-  subTitle: PropTypes.bool,
-  anchorId: PropTypes.string
-};
-
-Footer.propTypes = {
-  content: PropTypes.oneOfType([PropTypes.string, PropTypes.node])
+  subTitle: PropTypes.bool
 };
 
 export default ScrollableContent;
