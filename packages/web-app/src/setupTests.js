5645edf5b3ab580a66a6a7a7fb4bfb521452e8ce
Paul AUBERTIN <paul.aubertin.fr@gmail.com>
2026-07-23 19:47:49 +0200

    fixup

--- packages/web-app/src/setupTests.js
+++ packages/web-app/src/setupTests.js
@@ -4,8 +4,8 @@ import '@testing-library/jest-dom';
 
 // jsdom does not implement ResizeObserver, which is used by useMeasuredHeight
 // and the map components. Provide a no-op polyfill for the test environment.
-if (typeof globalThis.ResizeObserver === 'undefined') {
-  globalThis.ResizeObserver = class ResizeObserver {
+if (typeof window.ResizeObserver === 'undefined') {
+  window.ResizeObserver = class ResizeObserver {
     observe() {}
 
     unobserve() {}
